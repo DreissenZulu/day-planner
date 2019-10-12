de557776068f2e9cc7583afcdc17c68593a091e7
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-12 19:19:36 -0400

    Added function to adjust style based on current time

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var hourlyArray;
 var currentTime = moment();
+var textBlock = $(".col-8");
 
 // If a local hourly tasks exist, import them, otherwise initialize the array
 if (localStorage.getItem("localHourlyTasks")) {
@@ -8,4 +9,19 @@ if (localStorage.getItem("localHourlyTasks")) {
     hourlyArray = [];
 }
 
+// Write the current date by "DayofWeek, Month DayofMonth"
 $("#currentDay").text(`${currentTime.format('dddd, MMMM Do')}`);
+
+// The current hour is offset by 9 to match the array indicies, since 9AM is the first slot
+function updateCurrentScheduleTime() {
+    textBlock.removeClass('past present future');
+    $.each(textBlock, function(scheduleBlockHour) {
+        if(scheduleBlockHour < (currentTime.hour()-9)) {
+            $(this).addClass('past');
+        } else if (scheduleBlockHour == (currentTime.hour()-9)) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
+    })
+}
