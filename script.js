187a2bdb44ace9d3edb529c944d60a92fdbe5cd9
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-12 22:11:17 -0400

    Updates current time every minute, and updates style every hour

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var hourlyArray;
 var currentTime = moment();
+var currentHour = currentTime.hour();
 var textBlock = $(".col-8");
 var plannerTask = $("textarea");
+$.each(plannerTask, function () {
+    this.value = "";
+})
 
 // If a local hourly tasks exist, import them, otherwise initialize the array
 if (localStorage.getItem("localHourlyTasks")) {
@@ -16,10 +20,10 @@ $("#currentDay").text(`${currentTime.format('dddd, MMMM Do')}`);
 // The current hour is offset by 9 to match the array indicies, since 9AM is the first slot
 function updateCurrentScheduleTime() {
     textBlock.removeClass('past present future');
-    $.each(textBlock, function(scheduleBlockHour) {
-        if(scheduleBlockHour < (currentTime.hour()-9)) {
+    $.each(textBlock, function (scheduleBlockHour) {
+        if (scheduleBlockHour < (currentTime.hour() - 9)) {
             $(this).addClass('past');
-        } else if (scheduleBlockHour == (currentTime.hour()-9)) {
+        } else if (scheduleBlockHour == (currentTime.hour() - 9)) {
             $(this).addClass('present');
         } else {
             $(this).addClass('future');
@@ -30,15 +34,27 @@ function updateCurrentScheduleTime() {
 function updateLocalStorage() {
     event.preventDefault();
     let btnIndex = Number($(this).attr('id'));
-    hourlyArray[btnIndex] = plannerTask[btnIndex].value;
+    hourlyArray[btnIndex] = {
+        time: $(".hour")[btnIndex].textContent.trim(),
+        task: plannerTask[btnIndex].value
+    }
     localStorage.setItem("localHourlyTasks", JSON.stringify(hourlyArray));
 }
 
 function writeCurrentTasks() {
-    $.each(hourlyArray, function(i) {
-        plannerTask[i].value = hourlyArray[i];
+    $.each(hourlyArray, function (i) {
+        if (hourlyArray[i]) {
+            plannerTask[i].value = hourlyArray[i].task;
+        }
     })
 }
+
+setInterval(function() {
+    currentTime = moment();
+    if (currentHour < currentTime.hour()) {
+        updateCurrentScheduleTime();
+    }
+}, 60000)
 
 updateCurrentScheduleTime();
 writeCurrentTasks();
